0d7d880c53b5cb40d6f2e9fac67fff5b97b30d14
elBichon <reuglewiczjeanedouard@gmail.com>
2016-02-17 20:09:48 +0100

    première version du menu

---
+++ includes/menu.php
@@ -0,0 +1,4 @@
+<ul class="nav nav-tabs">
+    <li role="monCompte" class="active"><a href="compte.php">Compte</a></li>
+    <li role="deconnexion"><a href="deconnexion.php">Deconnexion</a></li>
+</ul>

58563b2d71b74adbfeccc4fb354a2f7645db1880
elBichon <reuglewiczjeanedouard@gmail.com>
2016-02-17 20:04:14 +0100

    la page de déconnexion

---
+++ deconnexion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Page de déconnexion</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+        </header>
+        <h1>Confirmation de ton inscription</h1>
+            <?php  include("includes/connect.php"); ?>
+        <section id="deconnexion">
+        <?php
+            session_start();
+            $_SESSION = array();
+            session_destroy();
+            setcookie('login', '');
+            setcookie('pass_hache', '');
+        ?>
+        </section>
+
+
+
+    <footer>
+            <?php  include("includes/footer.php"); ?>
+    </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+    </body>
+</html>

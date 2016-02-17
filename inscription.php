d18458e3c34e468d73ec39c5da03cccd7005a6d6
elBichon <reuglewiczjeanedouard@gmail.com>
2016-02-17 20:05:07 +0100

    la page d'inscription, envoi dans la bdd avec ses sécurités à modifier pbme jointure ville

---
+++ inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>inscription</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+        </header>
+        <h1>Inscription</h1>
+            <?php  include("includes/connect.php"); ?>
+        <section id="InscriptionSuite">
+            <?php  include("includes/verificationInscription.php"); ?>
+            <?php  include("includes/mailConfirmationInscription.php"); ?>
+
+
+        </section>
+
+
+    <footer>
+            <?php  include("includes/footer.php"); ?>
+    </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+    </body>
+</html>

2e94f5917661b7ed5638371c04550d844848cd35
elBichon <reuglewiczjeanedouard@gmail.com>
2016-02-17 20:09:23 +0100

    envoi du mail pour de made de confirmation

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<section id="footer">
+    <a href='codeOublie.php'>code oublié</a>
+    <a href='cgu.php'>conditions générales d'utilisation</a>
+</section>
---
+++ includes/mailConfirmationInscription.php
@@ -0,0 +1,5 @@
+<?php
+$message = "Bienvenue à FREE CITIZEN, tu es à un seul clic de ton réveil, pour cela, il te suffit de http://localhost:8888/cassiopee/freeCitizen/freeCitizenConfirmInscription.php confirmer ton inscription";
+        $titre = "confirmation d'inscription à FREE CITIZEN";
+        mail($email, $titre, $message);
+?>

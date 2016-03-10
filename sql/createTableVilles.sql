fa87494d38cb28b2a06d3afcfe8e43b9b0357319
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-10 16:54:44 +0100

    creation de la table des membres

---
+++ sql/createTableVilles.sql
@@ -0,0 +1,6 @@
+drop table if exists freeCitizenVilles;
+create table freeCitizenVilles (
+id integer not null primary key auto_increment,
+idMembre varchar(50) not null,
+ville varchar(50) not null
+) engine=innodb character set utf8 collate utf8_unicode_ci;

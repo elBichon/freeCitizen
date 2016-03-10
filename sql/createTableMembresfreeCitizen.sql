319ef695a1a959c828edb36c187f5ace9a6e4593
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-10 16:54:14 +0100

    creation de la table des membres

---
+++ sql/createTableMembresfreeCitizen.sql
@@ -0,0 +1,11 @@
+drop table if exists freeCitizenMembres;
+create table freeCitizenMembres (
+id integer not null primary key auto_increment,
+login varchar(50) not null,
+email varchar(50) not null,
+pass varchar(50) not null,
+cookies varchar(50),
+cgu varchar(50) not null,
+confirmation integer,
+connect integer
+) engine=innodb character set utf8 collate utf8_unicode_ci;

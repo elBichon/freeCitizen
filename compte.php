27155af26d93dfa8c431d71ae195db86a3d2a768
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-26 09:32:08 +0100

    mise à jour du système de vision en fonction de la connexion

--- compte.php
+++ compte.php
@@ -15,14 +15,15 @@
         <![endif]-->
     </head>
     <body>
-        <header>
-        </header>
-        <h1>Mon Compte</h1>
-        <?php include("includes/connect.php");?>
-        <?php include("includes/ville.php");?>
-        <?php include("includes/menu.php");?>
-        <section id="voirCompte">
 <?php
+    if ($_SESSION['id'] != 0) {
+        echo '<header>';
+        echo '</header>';
+        echo '<h1>Mon Compte</h1>';
+        require 'includes/connect.php';
+        include 'includes/ville.php';
+        include 'includes/menu.php';
+        echo '<section id="voirCompte">';
 
     $id = $_SESSION['id'];
    $reponse = $bdd->query('SELECT * FROM freeCitizenMembres WHERE id ="'.$id.'"');
@@ -31,19 +32,23 @@
         echo '<p>Mon Login: '. $donnees['login'] .'</p></br><p>Mon Email: '. $donnees['email'] .'</p></br>';
     }
     $reponse->closeCursor();
-    ?>
-        </section>
+    
+        echo '</section>';
 
-        <section id="modifierCompte">
-        </section>
+        echo '<section id="modifierCompte">';
+        echo '</section>';
 
-        <section id="detruireCompte">
-        </section>
-
-
-    <footer>
-            <?php  include("includes/footer.php"); ?>
-    </footer>
+        echo '<section id="detruireCompte">';
+        echo '</section>';
+    }
+    else {
+        echo "vous n'etes pas connecté";
+    }
+    
+    echo '<footer>';
+            require 'includes/footer.php';
+    echo '</footer>';
+?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
     </body>

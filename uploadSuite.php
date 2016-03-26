1ca206935fa1175657f0be88128cc7c267c84bd4
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-26 09:31:13 +0100

    mise à jour des formats acceptés

--- uploadSuite.php
+++ uploadSuite.php
@@ -9,7 +9,7 @@
             
             $infosfichier = pathinfo($_FILES['monfichier']['name']);
             $extension_upload = $infosfichier['extension'];
-            $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+            $extensions_autorisees = array('jpg', 'jpeg', 'png');
             if (in_array($extension_upload, $extensions_autorisees))
             {
                 move_uploaded_file($_FILES['monfichier']['tmp_name'], 'uploads/' . basename($_FILES['monfichier']['name']));

b4192762373d84c416524ae2dd2525066d556411
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-26 09:07:39 +0100

    création et réalisation d'une première version du formulaire d'upload de fichiers

---
+++ upload.php
@@ -0,0 +1,7 @@
+<form action="uploadSuite.php" method="post" enctype="multipart/form-data">
+    <p>
+        Formulaire d'envoi de fichier :<br />
+        <input type="file" name="monfichier" /><br />
+        <input type="submit" value="Envoyer le fichier" />
+    </p>
+</form>

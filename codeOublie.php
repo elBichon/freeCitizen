027c46db4d531fd4bf8d4b30f62dd6b9fb009676
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-26 08:48:57 +0100

    dernière modification du fichier de renvoi du code oublié

---
+++ codeOublie.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>indexFreeCitizen</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/verificationEmail.js"></script>
+
+    </head>
+
+    <body>
+    <header>
+    </header>
+
+    <h1>FREE CITIZEN</h1>
+
+    <?php include("includes/connect.php"); ?>
+    <section id="codeOublie">
+        <h3>Donnez votre adresse email pour recevoir votre code</h3></br>
+
+
+<div class="">
+<form action="codeOublieSuite.php" method="post">
+<div id="emailCodeOublie">
+<label for="emailCodeOublie">Login</label>
+<input type="text" class="form-control" id="champEmailCodeOublie" placeholder="login" name="login" required>
+</div>
+<div class="error-message"></div></br>
+<div class="button">
+<button type="submit" id="envoyerEmailCodeOublie" class="btn btn-default">Envoyer</button>
+</div>
+</form>
+</div>
+
+
+
+    </section>
+
+    <footer>
+            <?php  include("includes/footer.php"); ?>
+    </footer>
+    </body>
+</html>

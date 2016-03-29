23af68639461e1c3127bf49b25431c02038c4167
elBichon <reuglewiczjeanedouard@gmail.com>
2016-03-29 18:34:25 +0200

    dernières modifications

--- includes/menu.php
+++ includes/menu.php
@@ -2,3 +2,16 @@
     <li role="monCompte" class="active"><a href="compte.php">Compte</a></li>
     <li role="deconnexion"><a href="deconnexion.php">Deconnexion</a></li>
 </ul>
+
+<?php $reponse = $bdd->query('SELECT * FROM freeCitizenMembres WHERE login = "'.$ville.'"');
+while ($donnees = $reponse->fetch())
+{
+    echo '<p>id: '. $donnees['id'] .'</p></br>';
+    echo '<form method="post" action="deconnexion.php">';
+    echo '<input type="text" id="id" value="toto" name="id">';
+    echo '</form>';
+}
+$reponse->closeCursor();
+?>
+
+

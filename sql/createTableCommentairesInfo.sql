0da088cfdeb185e25c613f7755b0fb12956e5302
elBichon <reuglewiczjeanedouard@gmail.com>
2016-05-06 18:34:31 +0200

    Update createTableCommentairesInfo.sql

--- sql/createTableCommentairesInfo.sql
+++ sql/createTableCommentairesInfo.sql
@@ -1,3 +1,10 @@
+/*creation de la table d'insertion des infos
+identiofiant numerique
+titre
+date de post
+texte a inserer
+*/
+
 drop table if exists freeCitizenCommentairesInfos;
 create table freeCitizenCommentairesInfos (
 id integer not null primary key auto_increment,
@@ -5,7 +12,6 @@ titre varchar(50) not null,
 idInfo integer not null,
 datePost datetime not null,
 idCommentateur integer  not null,
-
 texte text not null
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 

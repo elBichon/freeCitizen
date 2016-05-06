6a0f6c3d982e12ac3b39e3021c5738ea51ce83bc
elBichon <reuglewiczjeanedouard@gmail.com>
2016-05-06 17:55:31 +0200

    Update mailConfirmationInscription.php

--- includes/mailConfirmationInscription.php
+++ includes/mailConfirmationInscription.php
@@ -1,3 +1,8 @@
+//envoi automatique d'un mail
+//se connecter au site via le lien fourni
+//confirmer son adresse mail
+//éviter l'insertion de fausses adresses mails dans la bdd membres
+
 <?php
 $message = "Bienvenue à FREE CITIZEN, tu es à un seul clic de ton réveil, pour cela, il te suffit de http://localhost:8888/cassiopee/freeCitizen/freeCitizenConfirmInscription.php confirmer ton inscription";
         $titre = "confirmation d'inscription à FREE CITIZEN";

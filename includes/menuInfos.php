0b5f04f6eac57e7f23ebe7c5fc9035be5dd27569
elBichon <reuglewiczjeanedouard@gmail.com>
2016-05-06 17:57:13 +0200

    Update menuInfos.php

--- includes/menuInfos.php
+++ includes/menuInfos.php
@@ -1,3 +1,8 @@
+//menu inclus sur les projets, events, produits, jobs, infos
+//permet la navigation avec la une des sujets les mieux notés
+//la recherche thématique
+//l'ajout de nouveaux billets
+
 <ul class="nav nav-tabs">
     <li role="infos" class="active"><a href="<?php echo $mieuxNotes; ?>">les mieux notés</a></li>
     <li role="recherche" class="active"><a href="<?php echo $recherche; ?>">Rechercher</a></li>

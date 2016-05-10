38f6fba3f8e69295e3f98f8f0ad68bbd8704748f
elBichon <reuglewiczjeanedouard@gmail.com>
2016-05-10 17:37:32 +0200

    Update freeCitizenConfirmInscriptionSuite.php

--- freeCitizenConfirmInscriptionSuite.php
+++ freeCitizenConfirmInscriptionSuite.php
@@ -1,3 +1,5 @@
+
+//suite et verifications de l inscription
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -20,6 +22,8 @@
             <?php  include("includes/connect.php"); ?>
         <section id="InscriptionSuiteConfirmation">
             <?php
+            
+            //appel aux variables et echappements des posts du formulaire
                 $i = 0;
                 $email = htmlspecialchars($_POST['email']);
                 $query1=$bdd->prepare('SELECT COUNT(*) AS nbr FROM freeCitizenMembres WHERE email =:email');
@@ -28,18 +32,23 @@
                 $mail_free=($query1->fetchColumn()==0)?1:0;
                 $query1->CloseCursor();
                 
+                //verification que l adresse mail est libre
                 if($mail_free){
                     $email_erreur1 = "Votre adresse email n'est pas enregistrée";
                     $i++;
                     echo $email_erreur1;
                     echo "</br>";
                 }
+                
+                //verification que l adresse a un format valide
                 if (!preg_match("#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#", $email) || empty($email)){
                     $email_erreur2 = "Votre adresse E-Mail n'a pas un format valide";
                     $i++;
                     echo $email_erreur2;
                     echo "</br>";
                 }
+                
+                //confirmation que l adresse est vraie via le mail de verification
                 else{
                     $req=$bdd->prepare("UPDATE freeCitizenMembres SET confirmation = 1 WHERE email = :email" );
                     $req->bindParam(":email",$_POST['email']);

82276ec51d6798967834c5916f051b7ab02be9cc
elBichon <reuglewiczjeanedouard@gmail.com>
2016-05-11 09:31:41 +0200

    Update createTableJob.sql

--- sql/createTableJob.sql
+++ sql/createTableJob.sql
@@ -12,6 +12,7 @@ ville varchar(50) not null,
 type varchar(50) not null,
 statut varchar(50) not null,
 idAuteur integer  not null,
+votes integer  not null,
 descriptif text not null
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 

9b924ebec39800a1ad2ab774949f5c48f61cf605
elBichon <reuglewiczjeanedouard@gmail.com>
2016-05-23 13:25:34 +0200

    mise à jour des balises title

--- codeOublie.php
+++ codeOublie.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>indexFreeCitizen</title>
+        <title>free citizen code oublié</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
--- membresSuite.php
+++ membresSuite.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>mon compte</title>
+        <title>free citizen membres</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
